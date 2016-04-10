99f8ba93b672c5f391a2a7adaef55e512adaa08e
XiaoBin DENG <XDeng4@slb.com>
2016-04-10 16:21:32 +0800

    Taxi fee, v2

--- taxi-fee-mocha.js
+++ taxi-fee-mocha.js
@@ -1,9 +1,8 @@
 //我们考虑出租车计价问题，
 // v1: 出租车的运价是每公里0.8元，
-// v2: 八公里起会加收50%的每公里运价，
+// v2: 八公里起会加收50%的每公里运价，计价的时候司机会四舍五入只收块块钱。
 // v3: 起步价是两公里以内6块，
 // v4: 停车等待时加收每分钟0.25元，
-// v5: 最后计价的时候司机会四舍五入只收块块钱。
 
 var should = require('should');
 var fee = require('./taxi-fee')
@@ -12,4 +11,17 @@ describe('Taxi fee', function(){
 	it('should be 4 yuan when 5 km distance', function(){
 		fee.taxiFee(5).should.equal(4);
 	});
+
+	it('should be 10 yuan when 8 km distance', function(){
+		fee.taxiFee(8).should.equal(10);
+	});
+
+	it('should be 12 yuan when 10 km distance', function(){
+		fee.taxiFee(10).should.equal(12);
+	});
+
+	it('should be 13 yuan when 11 km distance', function(){
+		fee.taxiFee(11).should.equal(13);
+	});
+
 });
--- taxi-fee.js
+++ taxi-fee.js
@@ -1,5 +1,12 @@
+var base = 0.8;
+var threshPrice = 8;
+
 var taxiFee = function(distance){
-	return (0.8*distance);
+	var price = base*distance;
+	if (distance >= threshPrice){
+		price *= 1.5;
+	}
+	return Math.round(price);
 };
 
 module.exports.taxiFee = taxiFee;
